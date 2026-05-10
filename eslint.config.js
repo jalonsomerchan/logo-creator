cbe88cf2eb5b74cf9e035b084b2c734c7af2f6a7
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-10 18:41:02 +0200

    Lint only optimized editor

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals';
 
 export default [
   {
-    ignores: ['dist/**', 'src/js/app2.js', 'src/js/app3.js', 'src/js/app4.js', 'src/js/app5.js', 'src/js/app6.js', 'src/js/app7.js'],
+    ignores: ['dist/**', 'src/js/app2.js', 'src/js/app3.js', 'src/js/app4.js', 'src/js/app5.js', 'src/js/app6.js', 'src/js/app7.js', 'src/js/app8.js'],
   },
   js.configs.recommended,
   {
